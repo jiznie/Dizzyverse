a82619456a551efa6267a923fcd45ce0cb9b1ea9
jiznie <dhizzzzy.gomez@gmail.com>
2024-12-10 09:47:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach(item => {
+  item.addEventListener('click', () => {
+    gridItems.forEach(otherItem => {
+      otherItem.classList.remove('zoomed');
+    });
+    item.classList.toggle('zoomed');
+  });
+});
+
